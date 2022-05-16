4c6fe79fbdf682bbac84551a0e4dfad0bc3c55a2
manismk <manismkmech@gmail.com>
2022-05-16 22:33:45 +0530

    feat: followers post filter

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,32 @@
+import { useEffect, useState } from "react";
 import { PostCard, SuggestionCard } from "../../components/";
-import { usePosts } from "../../context";
+import { usePosts, useUser } from "../../context";
 import "./home.css";
 
 export const Home = () => {
   const { posts } = usePosts();
+  const { allUsers } = useUser();
+  const [followersPost, setFollowersPost] = useState([]);
+  useEffect(() => {
+    setFollowersPost(
+      posts.posts.filter((post) =>
+        allUsers?.currentUser?.following?.includes(post.uid)
+      )
+    );
+  }, [posts, allUsers.currentUser]);
 
   return (
     <div className="container">
       <div className="post--container">
-        {posts.posts.map((post) => {
-          return <PostCard post={post} key={post.postId} />;
-        })}
+        {followersPost.length > 0 ? (
+          followersPost.map((post) => {
+            return <PostCard post={post} key={post.postId} />;
+          })
+        ) : (
+          <p className="text--center para--md text--bold">
+            No post from following users.
+          </p>
+        )}
       </div>
       <div className="suggestion--container">
         <SuggestionCard />
