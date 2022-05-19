c665569ffa1058ec18c3156699198124a5518112
manismk <manismkmech@gmail.com>
2022-05-19 13:43:42 +0530

    refactor:added auth routes to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import { routes } from "./constant";
+import { Login, SignUp } from "./pages";
+
 function App() {
-  return <div className="container-100"></div>;
+  return (
+    <Routes>
+      <Route path={routes.LOGIN_PAGE} element={<Login />} />
+      <Route path={routes.SIGNUP_PAGE} element={<SignUp />} />
+    </Routes>
+  );
 }
 
 export default App;
