84550ca9d593d3a097415d44ca3f7098d1b1986f
manismk <manismkmech@gmail.com>
2022-05-19 16:29:30 +0530

    chore: added barrel export for all folders

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+export { Navbar } from "./navbar/Navbar";
+export { InputPassword } from "./input/InputPassword";
+export { InputTextBox } from "./input/InputTextBox";
+export { RequiresAuth } from "./requires-auth/RequiresAuth";
+export { PostCard } from "./post-card/PostCard";
+export { SuggestionCard } from "./suggestion-card/SuggestionCard";
+export { EditProfileModal } from "./edit-profile-modal/EditProfileModal";
+export { CreatePostModal } from "./createPostModal/CreatePostModal";
+export { CommentContainer } from "./comment-card/CommentContainer";
+export { CommentInput } from "./comment-card/CommentInput";
+export { Comment } from "./comment-card/Comment";
+export { ReplyComment } from "./comment-card/ReplyComment";
+export { Loader } from "./loader/Loader";
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { SignUp } from "./signup/Signup";
+export { Login } from "./login/Login";
+export { Home } from "./home/Home";
+export { Profile } from "./profile/Profile";
+export { Saved } from "./saved/Saved";
+export { Explore } from "./explore/Explore";
+export { Post } from "./post/Post";
