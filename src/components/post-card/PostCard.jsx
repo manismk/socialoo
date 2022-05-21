0f0d9abdd07736f13db62a77227be0d547565440
manismk <61050821+manismk@users.noreply.github.com>
2022-05-21 12:11:35 +0530

    refactor: changed share icon to instagram

--- src/components/post-card/PostCard.jsx
+++ src/components/post-card/PostCard.jsx
@@ -4,7 +4,6 @@ import {
   ChatBubbleOutline,
   Favorite,
   FavoriteBorder,
-  Share,
 } from "@mui/icons-material";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
@@ -150,7 +149,32 @@ export const PostCard = ({ post }) => {
                 toast.success("Link copied to clipboard");
               }}
             >
-              <Share />
+              <svg
+                aria-label="Share Post"
+                className="_8-yf5 "
+                height="24"
+                role="img"
+                viewBox="0 0 24 24"
+                width="24"
+              >
+                <line
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  x1="22"
+                  x2="9.218"
+                  y1="3"
+                  y2="10.083"
+                ></line>
+                <polygon
+                  fill="none"
+                  points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334"
+                  stroke="currentColor"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                ></polygon>
+              </svg>
               <span>Share</span>
             </button>
           </div>
